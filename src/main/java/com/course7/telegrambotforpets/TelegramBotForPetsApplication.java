ea9a7a66ab011eb3a81d8e22bd3784c236c8cd8c
Саша <a.vasilievaaa@yandex.ru>
2022-08-20 19:15:15 +0300

    первый коммит

---
+++ src/main/java/com/course7/telegrambotforpets/TelegramBotForPetsApplication.java
@@ -0,0 +1,13 @@
+package com.course7.telegrambotforpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotForPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotForPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/course7/telegrambotforpets/TelegramBotForPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course7.telegrambotforpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotForPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
