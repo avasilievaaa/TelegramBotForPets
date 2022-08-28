03a3993b4d5731acc36d180e1e3bfd88e2eadad1
pavelkrivokorytov <paxa-net@mail.ru>
2022-08-28 18:10:16 +0300

    1st

--- src/main/java/com/course7/telegrambotforpets/listener/TelegramBotUpdatesListener.java
+++ src/main/java/com/course7/telegrambotforpets/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,8 @@ package com.course7.telegrambotforpets.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +30,18 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message().text().equals("/start")){
+              telegramBot.execute(new SendMessage(update.message().chat().id(), "privet").replyMarkup(new ReplyKeyboardMarkup(
+                      new String[]{"О приюте", "Как взять собаку из приюта"},
+                      new String[]{"Другой приют", "Прислать отчет о питомце", "Позвать волонтёра"})
+                      .oneTimeKeyboard(true)
+                      .resizeKeyboard(true)
+                      .selective(true)));
+
+            }
+            if (update.message().text().equals("/DOG")){
+                telegramBot.execute(new SendMessage(update.message().chat().id(), "Gav-gav"));
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
